5ba381997de1f4c8eeed52eb980076a2c781f2f8
park <pdw11@naver.com>
2023-12-30 23:25:26 +0900

    jsp 연습

--- 04.web/step08_ConnectionPool_t/src/dept/controller/GetDeptListController.java
+++ 04.web/step08_ConnectionPool_t/src/dept/controller/GetDeptListController.java
@@ -1,12 +1,17 @@
 package dept.controller; 
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dept.dao.DeptDAO;
+import dept.dto.Dept;
 
 @WebServlet("/getDeptList.do")
 public class GetDeptListController extends HttpServlet {
@@ -20,7 +25,26 @@ public class GetDeptListController extends HttpServlet {
 		 */
 		
 		// ?
-		
-		
+		ArrayList<Dept> deptList = null;
+			
+			try {
+				deptList = DeptDAO.getDeptList();
+				if (deptList != null) {
+					request.setAttribute("deptList", deptList);
+					request.getRequestDispatcher("dept/getDeptList.jsp").forward(request, response);
+					
+				}else {
+					request.setAttribute("error", "모든 부서 정보 미존재");
+					request.getRequestDispatcher("/errors/error.jsp").forward(request, response);
+				}
+				
+						
+				
+				
+			} catch (SQLException e) {
+//				e.printStackTrace();
+				request.setAttribute("error", "모든 부서 정보 미존재");
+				request.getRequestDispatcher("/errors/error.jsp").forward(request, response);
+			}	
 	}
 }
